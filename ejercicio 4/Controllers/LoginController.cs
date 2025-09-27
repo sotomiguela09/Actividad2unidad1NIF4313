7fbfd27ed9b786682823c064af5b3f17f8b7b7dc
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-26 22:40:04 -0400

    ejercicio 4

---
+++ ejercicio 4/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio_4.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        
+
+            [HttpGet]
+            public IActionResult Index() => View();
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public IActionResult Index(string usuario, string clave)
+            {
+                if (usuario == "Miguela" && clave == "A0224")
+                    return RedirectToAction("Bienvenido");
+                ViewBag.Mensaje = "Usuario incorrecto";
+                return View();
+            }
+
+        public IActionResult Bienvenido() => View();
+           
+        }
+    }
+
