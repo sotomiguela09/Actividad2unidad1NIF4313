e1f94e01db54a479e8eaaa2c30d29b4851a8769b
sotomiguela09 <sotomiguela09@gmail.com>
2025-09-26 22:00:17 -0400

    ejercicio 3

---
+++ ejercicio3/ejercicio3/Controllers/NumeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejercicio3.Controllers
+{
+    public class NumeroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Fibonacci() => View();
+        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Fibonacci(int numero)
+        {
+            bool pertenece = PerteneceAFibonacci(numero);
+            ViewBag.Mensaje = pertenece ? $"{numero} pertenece a Fibonacci" : $"{numero} NO pertenece a Fibonacci";
+            return View();
+        }
+
+        private bool PerteneceAFibonacci(int n)
+        {
+            int a = 0, b = 1;
+            while (a < n) { int t = a; a = b; b = t + b; }
+            return a == n;
+
+        }
+    }
+}
---
+++ ejercicio3/ejercicio3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ejercicio3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
